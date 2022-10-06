6ff12ce027a1ac9d7054410b5ffba8bb6c3c6ef3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-10-06 19:18:05 +0200

    update dependencies, simplify build script (#6)

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,15 @@
-import { packageExtension } from '@lvce-editor/package-extension'
-import fs from 'fs'
-import path, { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
+import { copyFiles, packageExtension } from '@lvce-editor/package-extension'
+import path, { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-
 const root = path.join(__dirname, '..')
-const extension = root
 
-fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
-
-fs.mkdirSync(path.join(root, 'dist'))
-
-fs.copyFileSync(join(root, 'README.md'), join(root, 'dist', 'README.md'))
-fs.copyFileSync(
-  join(extension, 'extension.json'),
-  join(root, 'dist', 'extension.json')
-)
-fs.copyFileSync(
-  join(extension, 'color-theme.json'),
-  join(root, 'dist', 'color-theme.json')
-)
+await copyFiles({
+  root,
+  files: ['README.md', 'extension.json', 'color-theme.json'],
+  outDir: join(root, 'dist'),
+})
 
 await packageExtension({
   highestCompression: true,
